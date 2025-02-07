3a4dc4ff70f69c86c0ffd19c7d40172cf604cf60
Mehdy Lefkir <mehdylefkir.work@gmail.com>
2025-02-07 12:55:14 +0000

    fix carma issues  and add tests

--- src/CARMA.jl
+++ src/CARMA.jl
@@ -79,13 +79,21 @@ end
 
 Convert the CARMA coefficients to Celerite coefficients.
 
+It is important to note that this assumes that the roots of the autoregressive polynomial are complex conjugate pairs.
+This means that for an odd order autoregressive polynomial p, the last root is real. We then have J = (p - 1) ÷ 2 + 1 Celerie terms in the covariance function.
+
+!!! note
+
+	If the roots are not complex conjugate pairs, the function will not throw an error!
+	
+
 # Arguments
 - `p::Int`: the order of the autoregressive polynomial
 - `rα::Vector{Complex}`: roots of the autoregressive polynomial
 - `β::Vector{Real}`: moving average coefficients
 - `σ²::Real`: the variance of the process   
 """
-function CARMA_celerite_coefs(p, rα, β, σ²)
+function CARMA_celerite_coefs(p::Int64, rα::Vector{Trα}, β::Vector{Tβ}, σ²::Tb) where {Trα <: Complex, Tβ <: Real, Tb <: Real}
 
 	T = eltype(β)
 	# check if the last root is real
@@ -165,7 +173,7 @@ Convert the roots of a polynomial to its coefficients.
 # Returns
 - `c::Vector{Complex}`: Coefficients of the polynomial.
 """
-function roots2coeffs(r)
+function roots2coeffs(r::Vector{T}) where T
 	P = fromroots(r)
 	return P.coeffs
 end
@@ -181,7 +189,7 @@ Convert the coefficients of a quadratic polynomial to its roots.
 # Returns
 - `r::Vector{Complex}`: Roots of the polynomial.
 """
-function quad2roots(quad)
+function quad2roots(quad::Vector{T}) where T
 	n = size(quad, 1)#length(quad)
 	r = zeros(Complex, n)
 	if n % 2 == 1
@@ -205,7 +213,7 @@ function quad2roots(quad)
 	return r
 end
 
-"""
+@doc raw"""
 	CARMA_covariance(τ, covariance::CARMA)
 
 Compute the covariance function of the CARMA model at time τ.
@@ -247,4 +255,135 @@ function CARMA_covariance(τ, covariance::CARMA)
 		variance += num / den
 	end
 	return real.(R) ./ real(variance) * covariance.σ²
+end
+
+"""
+	CARMA_normalisation(covariance::CARMA)
+
+Compute the normalisation constant of the CARMA model.
+
+"""
+function CARMA_normalisation(covariance::CARMA)
+	variance = 0.0
+
+	# compute the remaining terms
+	for rₖ in covariance.rα
+		num_1, num_2 = 0, 0
+		for (l, βₗ) in enumerate(covariance.β)
+			num_1 += βₗ * rₖ^(l - 1)
+			num_2 += βₗ * (-rₖ)^(l - 1)
+		end
+		num = num_1 * num_2
+
+		den = -2 * real(rₖ)
+		r_ = filter(x -> x != rₖ, covariance.rα)
+		for rⱼ in r_
+			den *= (rⱼ - rₖ) * (conj(rⱼ) + rₖ)
+		end
+
+		variance += num / den
+	end
+	return real(variance)
+end
+
+@doc raw""" 
+	sample_quad(p, q, rng, f_min, f_max)
+
+
+	Sample the quadratic coefficients of the CARMA model to ensure that the roots are complex conjugate pairs and that:
+	- The roots of the autoregressive polynomial are within the range [f_min, f_max]
+	- The roots of the moving average polynomial are within the range [f_min, f_max]
+	- The roots should be ordered in increasing order of magnitude.
+
+
+	# Arguments
+	- `p::Int64`: the order of the autoregressive polynomial
+	- `q::Int64`: the order of the moving average polynomial
+	- `rng::AbstractRNG`: the random number generator
+	- `f_min`: the minimum value of the roots
+	- `f_max`: the maximum value of the roots
+	
+	# Returns
+	- `qa::Vector{Real}`: the quadratic coefficients of the autoregressive polynomial
+	- `qb::Vector{Real}`: the quadratic coefficients of the moving average polynomial
+"""
+function sample_quad(p::Int64, q::Int64,rng::AbstractRNG,f_min,f_max)
+	buff = 0.0
+	f0_start = 0.0
+	f0_lower, f0_upper = f_min, f_max
+	# Define the LogNormal distribution for a_1
+	γ_dist = LogUniform(2 * f_min, 2 * f_max)
+	f0_dist = LogUniform(f0_lower, f0_upper)
+	a_3 = LogUniform(f_min, f_max)
+	qa = Vector(undef, p)
+	qb = Vector(undef, q)
+
+	if p % 2 == 0  # all roots are complex conjugates
+		# we first fill the quadratic coefficients with pair indices
+		for i in 2:2:p
+			qa[i] = rand(rng,γ_dist)
+		end
+		# then we fill the quadratic coefficients with odd indices
+		for (j, i) in enumerate(1:2:p-1)
+			if j == 1
+				buff = qa[i+1]^2 / 4
+				qa[i] = rand(rng,f0_dist) + buff
+			else
+				start_log = qa[i-2] - buff
+				buff = qa[i+1]^2 / 4
+				qa[i] = rand(rng,LogUniform(start_log, f0_upper)) + buff
+			end
+		end
+	else
+		qa[end] = rand(rng,a_3)
+		for i in 2:2:p-1
+			qa[i] = rand(rng,γ_dist)
+		end# then we fill the quadratic coefficients with odd indices
+		for (j, i) in enumerate(1:2:p-2)
+			# qa[i] ~ f0_dist + qa[i+1]^2 / 4
+			if j == 1
+				buff = qa[i+1]^2 / 4
+				qa[i] = rand(rng,f0_dist) + buff
+			else
+				start_log = qa[i-2] - buff
+				buff = qa[i+1]^2 / 4
+				qa[i] = rand(rng,LogUniform(start_log, f0_upper)) + buff
+			end
+		end
+	end
+	if q % 2 == 0  # all roots are complex conjugates
+		# we first fill the quadratic coefficients with pair indices
+		for i in 2:2:q
+			qb[i] = rand(rng,γ_dist)
+		end
+		# then we fill the quadratic coefficients with odd indices
+		for (j, i) in enumerate(1:2:q-1)
+			# qb[i] ~ f0_dist + qb[i+1]^2 / 4
+			if j == 1
+				buff = qb[i+1]^2 / 4
+				qb[i] =rand(rng,f0_dist) + buff
+			else
+				start_log = qb[i-2] - buff
+				buff = qb[i+1]^2 / 4
+				qb[i]  = rand(rng,LogUniform(start_log, f0_upper)) + buff
+			end
+		end
+	else
+		qb[end] = rand(rng,a_3)
+		for i in 2:2:q-1
+			qb[i]= rand(rng,γ_dist)
+		end
+		# then we fill the quadratic coefficients with odd indices
+		for (j, i) in enumerate(1:2:q-2)
+			if j == 1
+				buff = qb[i+1]^2 / 4
+				qb[i] =rand(rng,f0_dist) + buff
+			else
+				start_log = qb[i-2] - buff
+				buff = qb[i+1]^2 / 4
+				qb[i] = rand(rng,LogUniform(start_log, f0_upper)) + buff
+			end
+		end
+	end	
+	return qa, qb
 end
