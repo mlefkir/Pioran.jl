c31eab8c62c7cb23223859a44c2c798d00a2d149
Mehdy Lefkir <mehdylefkir.work@gmail.com>
2025-02-18 15:17:42 +0000

    add benchmark CI

--- benchmark/run_benchmark_celerite.jl
+++
@@ -1,5 +0,0 @@
-using PkgBenchmark
-import Pioran
-
-benchmarkpkg(Pioran, script = "benchmark/benchmark_celerite.jl", resultfile = "results_celerite_new.json")
-#benchmarkpkg(Pioran,script="benchmark/benchmarks_celerite_DRWCelerite.jl",resultfile="results_celerite_DRWCelerite.json")
